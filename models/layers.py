b3fa19d42d29448f4b9e9125f0556841fd380c34
Seratna <antares_tsao@hotmail.com>
2017-02-24 13:23:27 -0500

    finish all the layer classes

--- models/layers.py
+++ models/layers.py
@@ -71,6 +71,64 @@ class Convolution2D(Layer):
             return self.build(input_tensor)
 
 
+class DeConvolution2D(Layer):
+    """
+
+    """
+    def __init__(self,
+                 kernel_shape,
+                 output_shape,
+                 kernel=None,
+                 bias=None,
+                 strides=(1, 1, 1, 1),
+                 padding='SAME',
+                 activation=None,
+                 scope=''):
+        Layer.__init__(self)
+
+        self.kernel_shape = kernel_shape
+        self.output_shape = output_shape
+        self.kernel = kernel
+        self.bias = bias
+        self.strides = strides
+        self.padding = padding
+        self.activation = activation
+        self.scope = scope
+
+    def build(self, input_tensor):
+        # build kernel
+        if self.kernel:
+            assert self.kernel.get_shape() == self.kernel_shape
+        else:
+            self.kernel = tf.Variable(tf.truncated_normal(self.kernel_shape, stddev=0.1), name='kernel')
+
+        # build bias
+        window_height, window_width, num_output_channels, num_input_channels = self.kernel.get_shape()
+        if self.bias:
+            assert self.bias.get_shape() == (num_output_channels, )
+        else:
+            self.bias = tf.Variable(tf.constant(0.1, shape=[num_output_channels]), name='bias')
+
+        # convolution
+        deconv = tf.nn.conv2d_transpose(input_tensor,
+                                        self.kernel,
+                                        output_shape=self.output_shape,
+                                        strides=self.strides,
+                                        padding=self.padding)
+
+        # activation
+        if self.activation:
+            return self.activation(deconv + self.bias)
+        return deconv + self.bias
+
+    def call(self, input_tensor):
+        if self.scope:
+            with tf.variable_scope(self.scope) as scope:
+                return self.build(input_tensor)
+        else:
+            return self.build(input_tensor)
+
+
 class MaxPooling(Layer):
     """
 
@@ -100,23 +158,23 @@ class MaxPooling(Layer):
 
 class UnPooling(Layer):
     """
-        Unpool a max-pooled layer.
+    Unpool a max-pooled layer.
 
-        Currently this method does not use the argmax information from the previous pooling layer.
-        Currently this method assumes that the size of the max-pooling filter is same as the strides.
+    Currently this method does not use the argmax information from the previous pooling layer.
+    Currently this method assumes that the size of the max-pooling filter is same as the strides.
 
-        Each entry in the pooled map would be replaced with an NxN kernel with the original entry in the upper left.
-        For example: a 1x2x2x1 map of
+    Each entry in the pooled map would be replaced with an NxN kernel with the original entry in the upper left.
+    For example: a 1x2x2x1 map of
 
-            [[[[1], [2]],
-              [[3], [4]]]]
+        [[[[1], [2]],
+          [[3], [4]]]]
 
-        could be unpooled to a 1x4x4x1 map of
+    could be unpooled to a 1x4x4x1 map of
 
-            [[[[ 1.], [ 0.], [ 2.], [ 0.]],
-              [[ 0.], [ 0.], [ 0.], [ 0.]],
-              [[ 3.], [ 0.], [ 4.], [ 0.]],
-              [[ 0.], [ 0.], [ 0.], [ 0.]]]]
+        [[[[ 1.], [ 0.], [ 2.], [ 0.]],
+          [[ 0.], [ 0.], [ 0.], [ 0.]],
+          [[ 3.], [ 0.], [ 4.], [ 0.]],
+          [[ 0.], [ 0.], [ 0.], [ 0.]]]]
     """
     def __init__(self,
                  kernel_shape,
@@ -167,7 +225,8 @@ class Unfold(Layer):
 
     def build(self, input_tensor):
         num_batch, height, width, num_channels = input_tensor.get_shape()
-        return tf.reshape(input_tensor, [-1, height * width * num_channels])
+
+        return tf.reshape(input_tensor, [-1, (height * width * num_channels).value])
 
     def call(self, input_tensor):
         if self.scope:
@@ -213,7 +272,7 @@ class FullyConnected(Layer):
         Layer.__init__(self)
 
         self.output_dim = output_dim
-        self.weights = output_dim
+        self.weights = weights
         self.bias = bias
         self.activation = activation
         self.scope = scope
@@ -223,9 +282,10 @@ class FullyConnected(Layer):
 
         # build weights
         if self.weights:
-            assert self.weights.get_shape() == (input_dim, self.output_dim)
+            assert self.weights.get_shape() == (input_dim.value, self.output_dim)
         else:
-            self.weights = tf.Variable(tf.truncated_normal((input_dim, self.output_dim), stddev=0.1), name='weights')
+            self.weights = tf.Variable(tf.truncated_normal((input_dim.value, self.output_dim), stddev=0.1),
+                                       name='weights')
 
         # build bias
         if self.bias:
