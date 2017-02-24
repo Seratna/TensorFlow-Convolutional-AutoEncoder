baa264f4b43198c9c41b7d73aaafd1ff058aa6cd
Seratna <antares_tsao@hotmail.com>
2017-02-24 13:24:08 -0500

    add functions for preparing sessions

--- models/model.py
+++ models/model.py
@@ -9,6 +9,31 @@ class Model(object):
     def __init__(self):
         pass
 
+    @staticmethod
+    def start_new_session(sess):
+        saver = tf.train.Saver()  # create a saver
+        global_step = 0
+
+        sess.run(tf.global_variables_initializer())
+        print('started a new session')
+
+        return saver, global_step
+
+    @staticmethod
+    def continue_previous_session(sess, ckpt_file):
+        saver = tf.train.Saver()  # create a saver
+
+        with open(ckpt_file) as file:  # read checkpoint file
+            line = file.readline()  # read the first line, which contains the file name of the latest checkpoint
+            ckpt = line.split('"')[1]
+            global_step = int(ckpt.split('-')[1])
+
+        # restore
+        saver.restore(sess, 'saver/'+ckpt)
+        print('restored from checkpoint ' + ckpt)
+
+        return saver, global_step
+
 
 def main():
     pass
